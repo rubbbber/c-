be97c04b17e1405c72bcd751a3a242adcba4c801
rubbbber <100339034+rubbbber@users.noreply.github.com>
2022-09-04 12:26:56 +0800

    Add files via upload

---
+++ firstcase.cpp
@@ -0,0 +1,37 @@
+//引用测试
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = 10;
+//     int &a = i;
+//     a = 19;
+//     cout<<i<<endl;
+//     return 0;
+// }
+//输出19————引用即命名
+//测试2
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = 100,sum = 0;
+//     for(i = 0;i != 10;++i)
+//     {
+//         sum += i;
+//     }
+//     cout<<i<<" "<<sum<<endl;
+//     return 0;
+// }
+//测试1
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for(int i=0;i<5;i++)
+//     {
+//         cout<<"Hello world"<<endl;
+//     }
+//     return 0;
+// }
